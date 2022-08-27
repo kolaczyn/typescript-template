02e2e5ae812c91fbf2d6a7d71dba868bee1d74df
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2022-08-27 10:21:00 +0000

    add test:watch

--- src/greeter.test.ts
+++ src/greeter.test.ts
@@ -4,4 +4,7 @@ describe('greeter', () => {
   it('greets hello world', () => {
     expect(greeter('world')).toBe('Hello world!')
   })
+  it('greet Paweł', () => {
+    expect(greeter('Paweł')).toBe('Hello Paweł!')
+  })
 })
