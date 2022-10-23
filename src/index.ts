54d96a3d4ed2c7da1f8b64a0596ffa0f83c8568f
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2022-10-23 15:32:28 +0000

    final version setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // package setup
 export { greeter } from './greeter'
 
+export const addTwoNumbers = (a: number, b: number) => a + b
 // binary setup
 // import { greeter } from './greeter'
 // greeter('hello world')
