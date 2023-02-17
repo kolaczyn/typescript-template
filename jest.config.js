6fdbc1d3e566d808e8d8569e66dc804943a57863
Paweł Kołaczyński <kolaczyn@protonmail.com>
2023-02-17 20:06:30 +0000

    remove jest

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  testEnvironment: 'node',
-  modulePathIgnorePatterns: ['dist'],
-  transform: {
-    "^.+\\.(t|j)sx?$": ["@swc/jest"],
-  },
-}
